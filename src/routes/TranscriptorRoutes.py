f17dbe3acf2df42b40c481fe2a1a2b71697df0fc
DanielOQ <danielorozco0208@gmail.com>
2023-10-19 21:30:00 -0500

    arreglos en los modelos

--- src/routes/TranscriptorRoutes.py
+++ src/routes/TranscriptorRoutes.py
@@ -3,8 +3,9 @@ from datetime import datetime
 from src.services.Archivos import guardarAudio
 from src.services.ModelosTranscriptor import *
 from src.models.whisperConfig import whisperConfig
+
 import traceback
-import os
+import time
 
 # Logger
 from src.utils.Logger import Logger
@@ -26,19 +27,27 @@ def transcribir(modeloTranscriptor):
         nomArchivo = guardarAudio(request.remote_addr, archivo)
         #Procesar audio según el modelo
         if modeloTranscriptor == "whisper":
+            #Configuración del modelo
             modelo = request.form['modelo']
             dispositivo = request.form['dispositivo']
             tamLote = int(request.form['tamLote'])
             tipoComputo = request.form['tipoComputo']
-
             config = whisperConfig(modelo, dispositivo, tamLote, tipoComputo)
+            #Proceso de transcripción
+            start_time = time.time()
             transcripcion = transcribirWhisper(nomArchivo, config)
-            print(transcripcion)
+            end_time = time.time()
+            tiempo_transcurrido = end_time - start_time
+            return jsonify({'texto':transcripcion, 'tiempo':tiempo_transcurrido, 'success': True})
         elif modeloTranscriptor == "sphinx":
+            #Proceso de transcripción
+            start_time = time.time()
             transcripcion = transcribirSphinx(nomArchivo)
+            end_time = time.time()
+            tiempo_transcurrido = end_time - start_time
+            return jsonify({'texto':transcripcion, 'tiempo':tiempo_transcurrido, 'success': True})
         else:
-            return 'Modelo no encontrado'
-        return nomArchivo
+            return 'Modelo no encontrado'# Error
     except Exception as ex:
         Logger.add_to_log("error", str(ex))
         Logger.add_to_log("error", traceback.format_exc())
--- src/services/Archivos.py
+++ src/services/Archivos.py
@@ -6,6 +6,5 @@ import os
 def guardarAudio(ip, archivo):
     # Guardar el archivo en tu sistema de archivos
     nomArchivo = f'{ip}({datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}).wav'
-    print(nomArchivo)
     archivo.save(os.path.join("src/uploads",nomArchivo))
     return nomArchivo
